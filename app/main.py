cbeeecd90b30cb513e001f524722b92d30a89b40
Saib Ahmed <saibahmed7@gmail.com>
2025-03-05 17:06:39 +0600

    Modified files(main.py), Folder(storage) - path related bug fixed - now can be run using pythopn -m main.py Refs: 020

--- app/main.py
+++ app/main.py
@@ -31,4 +31,4 @@ app.include_router(uploader_router)
 app.include_router(chat_router)
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run("app.main:app", host="0.0.0.0", port=8000)
